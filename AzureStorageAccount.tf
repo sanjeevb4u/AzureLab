90570a1c01ed078ebbbf99653dca88de0323adea
sanjeevb4u <sanjeevb4u@gmail.com>
2024-04-27 03:43:56 +0530

    Changing All files

--- AzureStorageAccount.tf
+++ AzureStorageAccount.tf
@@ -1,17 +1,18 @@
 resource "azurerm_resource_group" "demo" {
   name     = "example-resources"
-  location = "west Europe"
+  location = "West Europe"
 }
 
-resource "azurerm_storage_account" "storageAccountDemo" {
+##  Demo now
+resource "azurerm_storage_account" "StorageAccountDemo" {
   name                     = "satestant000012"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
-  account_tire             = "standard"
+  account_tier             = "Standard"
   account_replication_type = "GRS"
 
   tags = {
-    video   = "azure"
+    video = "azure"
     channel = "MyTerraformTestLab"
   }
 }
